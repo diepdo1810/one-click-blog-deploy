d9c5f82ffb68093d55ace0b2b55f70d27b2b3d44
diepdo <diepdo1810@gmail.com>
2024-05-13 21:02:37 +0700

    first deploy

---
+++ routes/api/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('github')->user();
+
+    // $user->token
+});
