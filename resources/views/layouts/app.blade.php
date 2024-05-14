8d67598b517fe2e6cd3607b56529bd877297aa42
diepdp <diepdp@kaiyouit.com>
2024-05-14 13:57:39 +0700

    update deploy

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+
+    @yield('style')
+    <!-- Scripts -->
+    @vite(['resources/sass/user/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+        <main class="">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
